3e605c364c13148db2de5e030c37b8124f1b485a
Shrajan <biruvashrajan@gmail.com>
2026-07-16 19:07:49 +0530

    SQLAlchemy Engine created

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,3 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from config.settings import Settings
+from config.settings import settings
+
+
+DATABASE_URL = (
+    f"postgresql://{settings.database_user}:"
+    f"{settings.database_password}@"
+    f"{settings.database_host}:"
+    f"{settings.database_port}/"
+    f"{settings.database_name}"
+)
+
+engine = create_engine(DATABASE_URL)
