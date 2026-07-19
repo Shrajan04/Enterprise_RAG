a025761232fc733c0011045c0d884db05238b2df
Shrajan <biruvashrajan@gmail.com>
2026-07-19 18:09:39 +0530

    database engine created

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from config.settings import settings
-
+from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = (
     f"postgresql://{settings.database_user}:"
@@ -12,3 +12,12 @@ DATABASE_URL = (
 )
 
 engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
