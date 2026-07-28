a839ac62dd303b39d29318fdb205cb26788c3675
Shrajan <biruvashrajan@gmail.com>
2026-07-29 00:09:22 +0530

    hasing and verifying password

--- backend/utils/security.py
+++ backend/utils/security.py
@@ -1,3 +1,12 @@
 from passlib.context import CryptContext
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+# for hashing the password
+def hash_password(password: str)-> str:
+    return pwd_context.hash(password)
+
+#for verifying the password
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
