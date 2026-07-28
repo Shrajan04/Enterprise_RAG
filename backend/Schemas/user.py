6c7bdaa4fbbf1d9791c4ae7ac6d66120390fba70
Shrajan <biruvashrajan@gmail.com>
2026-07-29 00:07:20 +0530

    Schema updated

--- backend/Schemas/user.py
+++ backend/Schemas/user.py
@@ -1,7 +1,13 @@
 from pydantic import BaseModel, EmailStr
 
-
+#to create user
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
+
+#to send response to the user
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
