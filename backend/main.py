1d6a0cde7ff1775468422572b7e361d045480f85
Shrajan <biruvashrajan@gmail.com>
2026-07-29 00:12:36 +0530

    using Dependes to reduce rewriting of code

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from database.dependencies import get_db
+from schemas.user import UserCreate
 from config.settings import settings
 
-app = FastAPI(title=settings.app_name)
+app = FastAPI(
+    title=settings.app_name,
+    version=settings.app_version
+)
